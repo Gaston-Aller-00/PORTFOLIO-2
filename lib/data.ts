2543504007af02f60cbad732ba9a0724a17d1bab
Gaston <gastonallerdev@gmail.com>
2024-03-10 15:04:40 -0300

    data

--- lib/data.ts
+++ lib/data.ts
@@ -38,8 +38,7 @@ export const experiencesData = [
   {
     title: "Bootcamp Soy Henry",
     location: "Argentina, Bs as",
-    description:
-      "3 meses de Desarrollo intensivo, Programacion full stack",
+    description: "3 meses de Desarrollo intensivo, Programacion full stack",
     icon: React.createElement(LuGraduationCap),
     date: "2023",
   },
@@ -48,7 +47,7 @@ export const experiencesData = [
     location: "Coursera",
     description:
       "Curso Front-end de duracion de 6 meses, Orientado a React y Typescript ",
-      icon: React.createElement(LuGraduationCap),
+    icon: React.createElement(LuGraduationCap),
     // icon: React.createElement(CgWorkAlt),
     date: "2023",
   },
@@ -57,7 +56,7 @@ export const experiencesData = [
     location: "Coursera",
     description:
       "Curso Back-end de duracion de 8 meses,Orientado a python, MySQL y API REST ",
-      icon: React.createElement(LuGraduationCap),
+    icon: React.createElement(LuGraduationCap),
     // icon: React.createElement(FaReact),
     date: "2024",
   },
@@ -68,36 +67,42 @@ export const projectsData = [
     title: "Real Estate",
     description:
       "Real Estate donde los usuarios pueden reservar propiedades y crear publicaciones para poner su propiedad en venta. Desarrollo web full stack.",
-    tags: ["React", "Typescript","Next.js", "MongoDB", "Tailwind", "Prisma","Framer"],
+    tags: [
+      "React",
+      "Typescript",
+      "Next.js",
+      "MongoDB",
+      "Tailwind",
+      "Prisma",
+      "Framer",
+    ],
     imageUrl: corpcommentImg,
-    link: "https://real-estate-web-starterpack-lovat.vercel.app"
+    link: "https://real-estate-web-starterpack-lovat.vercel.app",
   },
   {
     title: "Book Store",
     description:
       "Librería en línea que permite a los usuarios comprar y cargar libros directamente en el sitio para venderlos. Desarrollo web full stack.",
-    tags: ["React",  "Vite", "Tailwind", "Redux"],
+    tags: ["React", "Vite", "Tailwind", "Redux"],
     imageUrl: rmtdevImg,
-    link: "https://mern-book-store-s72o.vercel.app"
+    link: "https://mern-book-store-s72o.vercel.app",
+  },
+  {
+    title: "K-GYM",
+    description: "A landing para un gimnasio local, minimalista y moderna.",
+    tags: ["React", "TypeScript", "Next.JS", "Tailwind", "Framer", "Swiper"],
+    imageUrl: wordanalyticsImg1,
+    link: "https://onepage-gym-next-13.vercel.app",
   },
   {
     title: "NEXCENT",
     description:
       "Landing page para vender servicios digitales, con un diseño minimalista y moderno",
-    tags: ["React",  "Tailwind", "Framer","React-scroll"],
+    tags: ["React", "Vite", "Tailwind", "Framer", "React-scroll"],
     imageUrl: wordanalyticsImg,
-    link: "https://modernagency.vercel.app"
-  },
-  {
-    title: "K-GYM",
-    description:
-      "A landing para un gimnasio local, minimalista y moderna.",
-    tags: ["React", "TypeScript","Next.JS" ,"Tailwind", "Framer","Swiper"],
-    imageUrl: wordanalyticsImg1,
-    link: "https://onepage-gym-next-13.vercel.app"
+    link: "https://modernagency.vercel.app",
   },
 ] as const;
-
 
 export const skillsData = [
   "HTML",
@@ -120,6 +125,6 @@ export const skillsData = [
   "Prisma",
   "React-Query",
   "MySQL",
-  
+
   "Framer Motion",
 ] as const;
