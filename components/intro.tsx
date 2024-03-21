29bdec188cedddb24a5504b4d2a1e182c8ef2625
Gaston <gastonallerdev@gmail.com>
2024-03-21 19:23:18 -0300

    fix

--- components/intro.tsx
+++ components/intro.tsx
@@ -97,12 +97,10 @@ export default function Intro() {
           Contáctame aquí{" "}
           <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
         </Link>
-
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="/CvGastonAller.pdf"
+          href="/CV.pdf"
           download
-          
         >
           Descargar CV{" "}
           <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
